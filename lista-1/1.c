042eaa299e75deb885c3e6ccfadfd2ad007bcd8f
Marina Stefane <128645734+marinastefane@users.noreply.github.com>
2023-03-23 15:39:17 -0300

    lista-introdução

---
+++ lista-1/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+    int main(){
+        int faces = 6, arestas = 12;
+        int vertices;
+
+        vertices = arestas + 2 - faces;
+            printf("O numero de vertices e %d", vertices);
+
+    }
