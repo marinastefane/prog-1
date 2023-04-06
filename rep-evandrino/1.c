2d280df601f8d7b913d672a09152e4804c624593
Marina Stefane <128645734+marinastefane@users.noreply.github.com>
2023-04-06 14:08:27 -0300

    rep-evandrino

---
+++ rep-evandrino/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+    int main(){
+        int num;
+            for ( num = 1; num <= 1000; num++)
+            {
+                printf("\nvalor:%i", num);
+            }
+    }
