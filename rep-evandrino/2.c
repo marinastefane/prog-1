dff0f8926953bea37844ddab142dfa146ca2fcc3
Marina Stefane <128645734+marinastefane@users.noreply.github.com>
2023-06-20 13:22:39 -0300

    rep-evandrino

--- rep-evandrino/2.c
+++ rep-evandrino/2.c
@@ -3,7 +3,7 @@
     int main(){
        
             for (int num = 0; num <= 1000; num++)
-            {
+                {
                 if (num % 2 == 0)
                 {
                     printf("\nvalor:%i", num);
